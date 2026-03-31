c9c35b0d2b992782c87354e4eccd0736719d93ef
Christopher Barr <barrchristopher801@gmail.com>
2026-03-30 23:52:08 -0600

    Add pin definitions for PN532 I2C and CC1101 SPI

---
+++ config.h
@@ -0,0 +1,9 @@
+// Pin definitions for PN532 I2C
+#define SDA_PIN 2
+#define SCL_PIN 3
+
+// Pin definitions for CC1101 SPI
+#define CC1101_CS_PIN 10
+#define CC1101_SCLK_PIN 13
+#define CC1101_MISO_PIN 12
+#define CC1101_MOSI_PIN 11
