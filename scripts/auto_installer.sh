34e5f2cda59af37e8f94984b87b21e301a937a11
Christopher Barr <barrchristopher801@gmail.com>
2026-03-30 23:48:46 -0600

    Add automated library installer script (bash)

---
+++ scripts/auto_installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Automated library installer script for CYD + Arduino Mega NFC/RF system
+
+# Install libraries
+arduino-cli lib install "SomeLibrary"
+# Add other necessary library installations here
